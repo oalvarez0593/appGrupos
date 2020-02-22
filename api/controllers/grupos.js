7f98c16834501a39eb9f4a2f8f82c6c1f622cf0f
Omar Alvarez <oalvarez@gbm.net>
2020-02-21 19:25:53 -0600

    Funcionalidad de crear grupos completada

--- api/controllers/grupos.js
+++ api/controllers/grupos.js
@@ -1,5 +1,5 @@
 const mongoosePaginate = require('mongoose-paginate');
-const User = require('../models/grupos');
+const Grupo = require('../models/grupos');
 const jwt = require('../jwt/jwt');
 
 function prueba(request, response) {
@@ -8,8 +8,58 @@ function prueba(request, response) {
 	});
 }
 
+function createGrupos(request, response) {
+	let grupoParams = request.body;
+
+	let grupo = new Grupo();
+	if (grupoParams.name && grupoParams.profesor && grupoParams.idGrupo && grupoParams.zona) {
+		grupo.name = grupoParams.name;
+		grupo.profesor = grupoParams.profesor;
+		grupo.idGrupo = grupoParams.idGrupo;
+		grupo.zona = grupoParams.zona;
+
+		Grupo.find({
+			$or: [
+				{ name: grupo.name }
+			]
+		}).exec((err, grupos) => {
+			if (err) return response.status(500).send({ message: 'Error en la petición de usuarios' });
+			if (grupos && grupos.length >= 1) {
+				return response.status(200).send({ message: `El grupo con nombre: ${grupo.name} que intenta registrar ya existe` });
+			} else {
+				grupo.save((err, grupoStored) => {
+					if (err) {
+						return response.status(500).send({
+							status: 500,
+							message: 'Error en el servidor',
+							sucess: false
+						})
+					}
+					if (grupoStored) {
+						return response.status(200).send({
+							status: 200,
+							grupo: grupoStored,
+						})
+					} else {
+						return response.status(404).send({
+							status: 404,
+							message: `No se ha logrado ingresar el grupo ${grupo.name}`
+						})
+					}
+				});
+			}
+		})
+
+	} else {
+		response.status(200).send({
+			message: 'Rellena todos los campos necesarios'
+		});
+	}
+}
+
 
 
 module.exports = {
-	prueba
+    prueba,
+    createGrupos
 }
--- api/routes/grupos.js
+++ api/routes/grupos.js
@@ -5,8 +5,8 @@ const md_auth = require('../middleware/authenticated');
 var api = express.Router();
 
 api.get('/grupos/prueba', GruposController.prueba);
-/* api.post('/user',  md_auth.getToken, UserController.createUser);
-api.put('/user/:id', md_auth.getToken, UserController.updateUser);
+ api.post('/grupos',  md_auth.getToken, GruposController.createGrupos);
+/* api.put('/user/:id', md_auth.getToken, UserController.updateUser);
 api.get('/users/:page?', md_auth.getToken, UserController.getUsers);
 api.get('/users/buscar/:palabra?', md_auth.getToken, UserController.getUsuarioMatch);
 api.delete('/user/:id', md_auth.getToken, UserController.deleteUser); */
